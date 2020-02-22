faa1648cd775638d8f2baa494b043d3bcc9f5434
flipflop133 <fbechet45@gmail.com>
2020-02-22 12:20:13 +0100

    first commit

---
+++ Projet_de_progra_specification.py
@@ -0,0 +1,65 @@
+Dict_board = {‘case_1’ : {’croiseur_1’,’ravitailleur _1’},’case_2 ‘ : {‘hub_1’}}
+Dict_army = {'player_1':{'alpha_1' : {‘type’ :’croiseur’, ‘PS’ :300, ‘Energie’ :200, ‘portée’ :2, …}, beta_1’ : {‘type’ :’ravitailleur’, ‘PS’ :100, ‘Energie’ :500, ‘, …}}}
+Dict3 : joueur_1_recrue = {‘croiseur’ : {‘PS’ :300, ‘capacité d’énergie’ :200, ‘portée’ :2, ‘cout’ :1000…} ‘ravitailleur : {, ‘PS’ :200, ‘capacité d’énergie’ :600, ‘cout’ :500, …}}}
+Dict_ordre = {‘joueur_1’ : {‘recrue’ : {},’amélioration’ : {}, …}
+‘Joueur_2’ : : {‘recrue’ : {},’amélioration’ : {}, …}}
+
+def display_board(dict_board):
+    """
+    display the board at the beginning of the game
+
+    parameters
+    ----------
+    dict_board: dictionnary with all the characteristic of the board (dict)
+    Version
+    -------
+
+    """
+def game():
+    """
+    start the game and play it
+
+    """
+def get_order():
+    """
+    ask the player for orders
+
+    return
+    ------
+    dict_order :dictionnary with all the order
+    """
+def create_board(fichier_source):
+    """
+    """
+    return dict_plat
+    
+def attack(dict_order[attack],dict_army):
+    """
+    execute attack order of each player
+
+    parameters
+    ----------
+    dict_order[attack]: dictionnary of attack order(dict)
+    dict_army : dictionnary with the unit of the two player(dict)
+    
+    return
+    ------
+    dict_army: dictionnary of the 2 army modified in result of the attack(dict)
+    """
+
+def move(dict_order[move]):
+    """
+    """
+
+def upgrade():
+    """
+    """
+def energy_transfert():
+    """
+    """
+def regenerate():
+    """
+    """
+def recruit_units():
+    """
+    """
