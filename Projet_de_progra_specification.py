f06537486d581291c3237d3d4b274c29ed8a34e7
flipflop133 <fbechet45@gmail.com>
2020-02-23 10:48:11 +0100

    update functions specs and data structures

--- Projet_de_progra_specification.py
+++ Projet_de_progra_specification.py
@@ -1,32 +1,62 @@
-Dict_board = {‘case_1’ : {’croiseur_1’,’ravitailleur _1’},’case_2 ‘ : {‘hub_1’}}
-Dict_army = {'player_1':{'alpha_1' : {‘type’ :’croiseur’, ‘PS’ :300, ‘Energie’ :200, ‘portée’ :2, …}, beta_1’ : {‘type’ :’ravitailleur’, ‘PS’ :100, ‘Energie’ :500, ‘, …}}}
-Dict3 : joueur_1_recrue = {‘croiseur’ : {‘PS’ :300, ‘capacité d’énergie’ :200, ‘portée’ :2, ‘cout’ :1000…} ‘ravitailleur : {, ‘PS’ :200, ‘capacité d’énergie’ :600, ‘cout’ :500, …}}}
-Dict_ordre = {‘joueur_1’ : {‘recrue’ : {},’amélioration’ : {}, …}
-‘Joueur_2’ : : {‘recrue’ : {},’amélioration’ : {}, …}}
+"""STRUCTURES
 
+#Dict1(board) :
+ dict_board = {'case_1' : {’croiseur_1’,’ravitailleur _1’},'case_2' : {‘hub_1’},'case_3' : {'peak_1'}}
+
+#Dict2(players units) : 
+ dict_army = {'player_1':{'alpha' : {‘type’ :’croiseur’, ‘PS’ :300, ‘Energie’ :200, ‘portée’ :2, …}, 'tango' : {‘type’ :’ravitailleur’, ‘PS’ :100, ‘Energie’ :500, ‘, …}} 'player_2' :{'bravo' : {'type' : 'croisseur, 'PS' : 200}}}
+
+#Dict3(available units to recruit -> one for each player) :
+ player_1_recruit = {‘croiseur’ : {‘PS’ :300, ‘capacité d’énergie’ :200, ‘portée’ :2, ‘cout’ :1000…} ‘ravitailleur : {, ‘PS’ :200, ‘capacité d’énergie’ :600, ‘cout’ :500, …}}}
+ player_2_recruit = {‘croiseur’ : {‘PS’ :600, ‘capacité d’énergie’ :300, ‘portée’ :2, ‘cout’ :1000…} ‘ravitailleur : {, ‘PS’ :200, ‘capacité d’énergie’ :600, ‘cout’ :500, …}}}
+
+#Dict4(orders) :
+ dict_ordre = {‘joueur_1’ : {‘recrue’ : {},’amélioration’ : {}, …}‘Joueur_2’ : : {‘recrue’ : {},’amélioration’ : {}, …}}
+
+"""
 def display_board(dict_board):
-    """
-    display the board at the beginning of the game
+    """Display the board at the beginning of the game
 
-    parameters
+    Parameters
     ----------
     dict_board: dictionnary with all the characteristic of the board (dict)
-    Version
+
+    Returns
+    -------
+
+    Versions
     -------
 
     """
-def game():
-    """
-    start the game and play it
+def game(color=True):
+    """Start the game and play it
+
+    Parameters
+    ----------
+    color: use color overall or not(bool)
+
+    Returns
+    -------
+
+    Versions
+    --------
 
     """
+
+def play_turn():
+
 def get_order():
-    """
-    ask the player for orders
+    """Ask the player for orders
 
-    return
-    ------
+    Parameters
+    ----------
+
+    Returns
+    -------
     dict_order :dictionnary with all the order
+
+    Versions
+    --------
     """
 def create_board(fichier_source):
     """
@@ -34,21 +64,33 @@ def create_board(fichier_source):
     return dict_plat
     
 def attack(dict_order[attack],dict_army):
-    """
-    execute attack order of each player
+    """Execute attack order of each player
 
-    parameters
+    Parameters
     ----------
     dict_order[attack]: dictionnary of attack order(dict)
     dict_army : dictionnary with the unit of the two player(dict)
     
-    return
-    ------
+    Returns
+    -------
     dict_army: dictionnary of the 2 army modified in result of the attack(dict)
+
+    Versions
+    -------
     """
 
-def move(dict_order[move]):
-    """
+def move(dict_order[move],dict_board):
+    """Moves the units on the board
+
+    Parameters
+    ----------
+    dict_order[move]: dictionnary of move order(dict)
+    dict_board : dictionnary with the units emplacements(dict)
+    Returns
+    -------
+    dict_board : dictionnary with the cases modified in result of the move(dict)
+    Versions
+    --------
     """
 
 def upgrade():
@@ -62,4 +104,22 @@ def regenerate():
     """
 def recruit_units():
     """
+    """
+
+def mining():
+    """Transfer energy from energy cases to player
+    """
+
+def compute_manhattan_distance():
+    """Compute the manhattan distance 
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    Versions
+    --------
+
     """
