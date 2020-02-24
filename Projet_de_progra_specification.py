4e32818fc266e2b7f361e9dd8aef1b2b9f15d129
flipflop133 <fbechet45@gmail.com>
2020-02-24 19:59:09 +0100

    update specifications

--- Projet_de_progra_specification.py
+++ Projet_de_progra_specification.py
@@ -1,13 +1,12 @@
 dict_board = {‘case_1’ : {’cruiser_1’,’tanker _1’},’case_2 ‘ : {‘hub_1’}}
-dict_army = {'player_1':{'alpha_1': {‘ship_type’ :’cruiser’, ‘HP’ :300, ‘energy’ :200, ‘distance’ :2, …}, beta_1’ : {‘ship_type’ :’tanker’, ‘HP’ :100, ‘energy’ :500, ‘, …}}}
+dict_army = {'player_1':{'alpha_1': {‘ship_type’ :’cruiser’, ‘HP’ :300, ‘energy’ :200, ‘distance’ :2, …}, beta_1’ : {‘ship_type’ :’tanker’, ‘HP’ :100, ‘energy’ :500, ‘, …}},'player_2' : {alpha3 : {'ship_type' : 'cruiser','HP':300}}}
 dict_recruit = {'player_1':{'research':{'energy_regen':0,'move_cost':2},‘cruiser’ : {‘HP’ :300, ‘energy-capacity’ :200, ‘distance’ :2, ‘cost’ :1000…} ‘tanker : {, ‘HP’ :200, ‘energy-capacity’ :600, ‘cost’ :500, …}},
                 'player_2':{'research':{'energy_regen':1,'move_cost':3},‘cruiser’ : {‘HP’ :300, ‘energy-capacity’ :200, ‘distance’ :2, ‘cost’ :1000…} ‘tanker : {, ‘HP’ :200, ‘energy-capacity’ :600, ‘cost’ :500, …}}}
 dict_ordre = {‘player_1’ : {‘recruit’ : {},’upgrade’ : {}, …}
               ‘player_2’ : : {‘recruit’ : {},’upgrade’ : {}, …}}
 
 def display_board(dict_board):
-    """
-    display the board at the beginning of the game
+    """display the board at the beginning of the game
 
     parameters
     ----------
@@ -15,33 +14,30 @@ def display_board(dict_board):
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
 
     """
 def game():
-    """
-    start the game and play it
+    """start the game and play it
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: François Bechet (v.1 24/02/20)
 
     """
 def get_order():
-    """
-    ask the player for orders
+    """ask the player for orders
 
     return
     ------
-    dict_order :dictionnary with all the order
+    dict_order: dictionnary with all the order
     
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
 def create_board(board_file):
-    """
-    take a file and change it into a board
+    """take a file and change it into a board
 
     parameters
     ----------
@@ -53,17 +49,16 @@ def create_board(board_file):
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
     
 def attack(dict_order[attack],dict_army):
-    """
-    execute attack order of each player and modify stats of each effected unit
+    """execute attack order of each player and modify stats of each effected unit
 
     parameters
     ----------
     dict_order[attack]: dictionnary of attack order(dict)
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
     
     return
     ------
@@ -71,115 +66,141 @@ def attack(dict_order[attack],dict_army):
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
 
 def move(dict_order[move],dict_board,dict_army):
-    """
-    execute move order of each player and modify board and stats of moving units
+    """execute move order of each player and modify board and stats of moving units
     
     prameters
     ---------
     dict_order[move]: dictionnary of move order(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
     
     return
     ------
-    dict_board : dictionnary with all the characteristic of the board (dict)
+    dict_board: dictionnary with all the characteristic of the board (dict)
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: François Bechet (v.1 24/02/20)
     """
 
 def upgrade(dict_order[upgrade],dict_army,dict_recruit):
-    """
-    execute the upgrage order of each player and modify the stats of each affected unit
+    """execute the upgrage order of each player and modify the stats of each affected unit
 
     parameters
     ----------
     dict_order[upgrade]: dictionnary of upgrade order(dict)
-    dict_army : dictionnary with the unit of the two player(dict)
-    dict_recruit : dictionnary with research and stat of new ship
+    dict_army: dictionnary with the unit of the two player(dict)
+    dict_recruit: dictionnary with research and stat of new ship
 
     return
     ------
-    dict_army : dictionnary with the unit of the two player(dict)
-    dict_recruit : dictionnary with research and stat of new ship(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
+    dict_recruit: dictionnary with research and stat of new ship(dict)
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: François Bechet (v.1 24/02/20)
 
     """
 def energy_transfert(dict_army,dict_order[energy_transfert]):
-    """
-    execute transfert order and modify affected unit's stat
+    """execute transfert order and modify affected unit's stat
     
     parameters
     ----------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
     dict_order[energy_transfert]:dictionnary of energy transfert order(dict)
     
     return
     ------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
 def regenerate(dict_army):
-    """
-    makes hub regenerate energy(at the end of the turn)
+    """makes hub regenerate energy(at the end of the turn)
 
     parameters
     ----------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
 
     return
     ------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
 def recruit_units(dict_recruit,dict_order[recruit],dict_army):
-    """
-    execute recruit order and add unit to the army
+    """execute recruit order and add unit to the army
 
     parameters
     ----------
-    dict_army : dictionnary with the unit of the two player(dict)
-    dict_recruit : dictionnary with research and stat of new ship(dict)
-    dict_order[recruit]:dictionnary of upgrade order(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
+    dict_recruit: dictionnary with research and stat of new ship(dict)
+    dict_order[recruit]: dictionnary of upgrade order(dict)
 
     return
     ------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
     
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: Dominik Everaert (v.1 24/02/20)
     """
 def energy_mining(dict_army,dict_order[energy_mining],dict_board):
-    """
-    execute mining order and modify affected unit's stat and energy peak's stat
+    """execute mining order and modify affected unit's stat and energy peak's stat
     
     parameters
     ----------
-    dict_army : dictionnary with the unit of the two player(dict)
-    dict_order[energy_mining]:dictionnary of energy mining order(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
+    dict_order[energy_mining]: dictionnary of energy mining order(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
     
     return
     ------
-    dict_army : dictionnary with the unit of the two player(dict)
+    dict_army: dictionnary with the unit of the two player(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
 
     Version
     −−−−−−−
-    specification : Dominik Everaert ( v.1 24/02/2020)
+    specification: François Bechet (v.1 24/02/20)
+    """
+
+def compute_manhattan_distance(x_shooter,y_shooter,x_target,y_target):
+    """compute the distance between a cruiser and its target
+
+    Parameters
+    ----------
+    x_shooter: coordinate x of the shooter(int)
+    y_shooter: coordinate y of the shooter(int)
+    x_target: coordinate x of the target(int)
+    y_target: coordinate y of the target(int)
+    Return
+    ------
+    distance: distance between the cruiser and the target(int)
+
+    Version
+    -------
+    specification: François Bechet (v.1 24/02/20)
+    """
+def play_turn(dict_board,dict_army,dict_recruit,dict_ordre):
+    """ manage each turn of the game by receiving the commands of each player
+
+    Parameters
+    ----------
+    dict_army: dictionnary with the unit of the two player(dict)
+    dict_order:dictionnary of players orders(dict)
+    dict_board: dictionnary with all the characteristic of the board (dict)
+    dict_recruit: dictionnary with research and stat of new ship(dict)
+
+    Version
+    -------
+    specification: François Bechet (v.1 24/02/20)
     """
