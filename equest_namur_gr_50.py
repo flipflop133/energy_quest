739c22591de8afd40c8cb7ede6b6dd4a2122e6dc
flipflop133 <48946818+flipflop133@users.noreply.github.com>
2020-03-06 10:46:27 +0100

    some fixes and specification changes

--- equest_namur_gr_50.py
+++ equest_namur_gr_50.py
@@ -7,10 +7,13 @@ def display_board(dict_board,height,width,player1,player2,dict_army):
     parameters
     ----------
     dict_board: dictionnary with all the characteristic of the board (dict)
-
+    height:height of the board(int)
+    width:width of the board(int)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     implementation: François Bechet (v.1 01/03/20)
 
     """
@@ -76,7 +79,7 @@ def game(play_game):
     player1 = input("who is player1 : ")
     player2 = input("who is player2 : ")
     # create dict_recruit
-    dict_recruit = {player1:{'cruiser':{'ship_type':'cruiser','hp':300,'energy-capacity':200, 'distance':2, 'cost':1000},'tanker':{'ship_type':'tanker','hp':200, 'energy-capacity':600, 'cost':500},'research':{'ship_type':'research','energy_regen':0,'move_cost':2}},player2:{'cruiser':{'ship_type':'cruiser','hp':300,'energy-capacity':200, 'distance':2, 'cost':1000},'tanker':{'ship_type':'tanker','hp':200, 'energy-capacity':600, 'cost':500},'research':{'ship_type':'research','energy_regen':0,'move_cost':2}}}
+    dict_recruit = {player1:{'cruiser':{'ship_type':'cruiser','hp':100,'energy_capacity':400, 'shooting_range':1,'move_cost':10,'shooting_cost':10 ,'cost':750},'tanker':{'ship_type':'tanker','hp':50, 'energy_capacity':600,'move_cost':0, 'cost':1000},'research':{'regeneration':0,'storage':0,'range':0,'move':0}},player2:{'cruiser':{'ship_type':'cruiser','hp':100,'energy_capacity':400, 'shooting_range':1,'move_cost':10,'shooting_cost':10 ,'cost':750},'tanker':{'ship_type':'tanker','hp':50, 'energy_capacity':600,'move_cost':0, 'cost':1000},'research':{'regeneration':0,'storage':0,'range':0,'move':0}}}
     # call the create_board function and store its return values
     board_values = create_board("board.txt",player1,player2)
     dict_board = board_values[0]
@@ -84,7 +87,8 @@ def game(play_game):
     width = board_values[2]
     
     # create dictionnary of the army
-    dict_army = {player1:{},player2:{}}
+    dict_army = board_values[3]
+    print(dict_army)
 
     # call the display_board function
     display_board(dict_board,height,width,player1,player2,dict_army)
@@ -108,13 +112,18 @@ def get_order(player1,player2):
         ATTACK ORDER(nom:*r-c=q where r,c is position of target and q are damages) : 'charlie:*10-15=23' 
         TRANSFER ORDER(nom:<r-c where the tanker take energy in the targeted hub; name1:>name2 where name1(tanker) gives energy to name2(cruiser or hub)) : alpha:<30-31 bravo:>charlie delta:>hub
 
+    parameters
+    ----------
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
+
     return
     ------
     dict_order: dictionnary with all the order
     
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     implementation: François Bechet (v.1 01/03/20)
     """
 
@@ -157,6 +166,8 @@ def create_board(board_file,player1,player2):
     parameters
     ----------
     board_file: file in which all the element needed for the board are(path)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     
     return
     ------
@@ -164,7 +175,7 @@ def create_board(board_file,player1,player2):
 
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     implementation: François Bechet (v.1 01/03/20)
     """
 
@@ -206,7 +217,12 @@ def create_board(board_file,player1,player2):
     dict_board['@%d-%d'%(key1_hub_2,key2_hub_2)] = {player2:{'hub':''}}
 
     # create dict_army
-    dic
+    # dict_army = {player1:{'hub':{'hp'=300,'current_energy'=1500,'energy_capacity'=1500,'regeneration'=25}}
+    print(dictFile['hubs'][0][2])
+    hub_1 = {'hp':int(dictFile['hubs'][0][2]),'current_energy':int(dictFile['hubs'][0][3]),'energy_capacity':int(dictFile['hubs'][0][3]),'regeneration':int(dictFile['hubs'][0][4])}
+    hub_2 = {'hp':int(dictFile['hubs'][1][2]),'current_energy':int(dictFile['hubs'][1][3]),'energy_capacity':int(dictFile['hubs'][1][3]),'regeneration':int(dictFile['hubs'][1][4])}
+    dict_army = {player1:{'hub':hub_1},player2:{'hub':hub_2}}
+
     # add peaks to the board's dictionnary
     for i in range(len(dictFile['peaks'])):
         peak = 'peak_'
@@ -216,7 +232,7 @@ def create_board(board_file,player1,player2):
         dict_board['@%d-%d'%(int(list_peak[0]),int(list_peak[1]))] = {'peak':{peak:{'energy':energy}}}
 
     # returns
-    return dict_board,height,width
+    return dict_board,height,width,dict_army
     
 def attack(dict_order,dict_army,player1,player2):
     """execute attack order of each player and modify stats of each effected unit
@@ -225,6 +241,8 @@ def attack(dict_order,dict_army,player1,player2):
     ----------
     dict_order[attack]: dictionnary of attack order(dict)
     dict_army: dictionnary with the unit of the two player(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     
     return
     ------
@@ -232,7 +250,7 @@ def attack(dict_order,dict_army,player1,player2):
 
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     """
 
 def move(dict_order,dict_board,dict_army,player1,player2):
@@ -243,6 +261,8 @@ def move(dict_order,dict_board,dict_army,player1,player2):
     dict_order[move]: dictionnary of move order(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
     dict_army: dictionnary with the unit of the two player(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     
     return
     ------
@@ -250,7 +270,8 @@ def move(dict_order,dict_board,dict_army,player1,player2):
 
     Version
     −−−−−−−
-    specification: François Bechet (v.1 24/02/20)
+    specification: François Bechet (v.2 24/02/20)
+    implementation: François Bechet (v.1 25/02/20)
     """
     # extract the move order from dict_order and change the position unit in dict_board
     moveList = ''
@@ -280,6 +301,7 @@ def move(dict_order,dict_board,dict_army,player1,player2):
                         tempDict[player][moveList[0]].update(unit)
                         dict_board[case].update(tempDict)
     
+    # TODO : make the cruiser for the move
     return dict_board
     
 def upgrade(dict_order,dict_army,dict_recruit,player1,player2):
@@ -289,7 +311,9 @@ def upgrade(dict_order,dict_army,dict_recruit,player1,player2):
     ----------
     dict_order[upgrade]: dictionnary of upgrade order(dict)
     dict_army: dictionnary with the unit of the two player(dict)
-    dict_recruit: dictionnary with research and stat of new ship
+    dict_recruit: dictionnary with research and stat of new ship(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
 
     return
     ------
@@ -298,7 +322,7 @@ def upgrade(dict_order,dict_army,dict_recruit,player1,player2):
 
     Version
     −−−−−−−
-    specification: François Bechet (v.1 24/02/20)
+    specification: François Bechet (v.2 24/02/20)
     implementation: Dominik Everaert (v.1 05/03/20)
     """
      # extract the upgrade order from dict_order and change the stat of unit
@@ -317,72 +341,81 @@ def upgrade(dict_order,dict_army,dict_recruit,player1,player2):
             print(upgradeList)
 
             # choose which upgrade must be done
+            print(dict_recruit)
             if upgradeList[1] == 'regeneration':
-                if dict_army[player][hub][1]>=750 and dict_recruit[player][research][energy_regen] < 10 :
-                     dict_army[player][hub][4]+=5
-                     dict_recruit[player][research][energy_regen]+=1
+                if dict_army[player]['hub']['current_energy']>=750 and dict_recruit[player]['research']['regeneration'] < 10 :
+                     dict_army[player]['hub']['regeneration']+=5
+                     dict_recruit[player]['research']['regeneration']+=1
 
                 else:
                     print("you can't upgrade energy regenaretion")
                 
             elif upgradeList[1] == 'storage':
                 print(dict_army)
-                if dict_army[player][hub][1]>=600 and dict_recruit[player][research][energy_capacity] < 12 :
-                    dict_recruit[player][research][energy_capacity]+=1
-                    dict_recruit[player][tanker][energy_capacity]+=100
+                print(dict)
+                if dict_army[player]['hub']['current_energy']>=600 and dict_recruit[player]['research']['storage'] < 12 :
+                    dict_recruit[player]['research']['storage']+=1
+                    dict_recruit[player]['tanker']['energy_capacity']+=100
                     for i in range(len(dict_army[player])):
-                        if dict_army[player][i][ship_type] == 'tanker' :
-                            dict_army[player][i][energy_capacity]+=100
+                        print(i)
+                        if dict_army[player][i]['ship_type'] == 'tanker' :
+                            dict_army[player][i]['energy_capacity']+=100
 
                 else:
-                    print('you can\'t  upgrade energy capacity')
+                    print("you can't upgrade energy capacity")
                 
             elif upgradeList[1] == range:
-                if dict_army[player][hub][1]>=400 and dict_recruit[player][research][shooting_range] < 5 :
-                    dict_recruit[player][research][shooting_range]+=1
-                    dict_recruit[player][cruiser][shooting_range]+=1
+                if dict_army[player]['hub']['current_energy']>=400 and dict_recruit[player]['research']['range'] < 5 :
+                    dict_recruit[player]['research']['range']+=1
+                    dict_recruit[player]['cruiser']['shooting_range']+=1
                     for i in range(len(dict_army[player])):
-                        if dict_army[player][i][ship_type] == 'cruiser' :
-                            dict_army[player][i][shooting_range]+=1
+                        if dict_army[player][i]['ship_type'] == 'cruiser' :
+                            dict_army[player][i]['shooting_range']+=1
 
                 else:
-                    print('you can\'t  upgrade shooting range')
+                    print("you can't upgrade shooting range")
 
             elif upgradeList[1] == move:
-                if dict_army[player][hub][1]>=500 and dict_recruit[player][research][move_cost] < 5 :
-                    dict_recruit[player][research][move_cost]+=1
-                    dict_recruit[player][cruiser][move_cost]-=1
+                if dict_army[player]['hub']['current_energy']>=500 and dict_recruit[player]['research']['move'] < 5 :
+                    dict_recruit[player]['research']['move']+=1
+                    dict_recruit[player]['cruiser']['move_cost']-=1
                     for i in range(len(dict_army[player])):
-                        if dict_army[player][i][ship_type] == 'cruiser' :
-                            dict_army[player][i][move_cost]-=1
+                        if dict_army[player][i]['ship_type'] == 'cruiser' :
+                            dict_army[player][i]['move_cost']-=1
 
                 else:
-                    print('you can\'t  upgrade movement ')
+                    print("you can't upgrade movement")
 
     return dict_army,dict_recruit 
 
-def energy_transfert(dict_army,dict_order):
+def energy_transfert(dict_army,dict_order,dict_board,player1,player2):
     """execute transfert order and modify affected unit's stat
     
     parameters
     ----------
     dict_army: dictionnary with the unit of the two player(dict)
     dict_order[energy_transfert]:dictionnary of energy transfert order(dict)
+    dict_board: dictionnary with all the characteristic of the board (dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     
     return
     ------
     dict_army: dictionnary with the unit of the two player(dict)
+    dict_board: dictionnary with all the characteristic of the board (dict)
 
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     """
-def regenerate(dict_army):
+def regenerate(dict_army,player1,player2):
     """makes hub regenerate energy(at the end of the turn)
 
     parameters
     ----------
     dict_army: dictionnary with the unit of the two player(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
 
     return
     ------
@@ -401,6 +434,8 @@ def recruit_units(dict_order,dict_army,player1,player2,dict_board,dict_recruit):
     dict_army: dictionnary with the unit of the two player(dict)
     dict_recruit: dictionnary with research and stat of new ship(dict)
     dict_order[recruit]: dictionnary of upgrade order(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
 
     return
     ------
@@ -408,7 +443,7 @@ def recruit_units(dict_order,dict_army,player1,player2,dict_board,dict_recruit):
     
     Version
     −−−−−−−
-    specification: Dominik Everaert (v.1 24/02/20)
+    specification: Dominik Everaert (v.2 24/02/20)
     implementation: François Bechet (v.1 01/03/20)
     """
 
@@ -438,7 +473,7 @@ def recruit_units(dict_order,dict_army,player1,player2,dict_board,dict_recruit):
             
     return dict_army, dict_board
 
-def energy_mining(dict_army,dict_order,dict_board):
+def energy_mining(dict_army,dict_order,dict_board,player1,player2):
     """execute mining order and modify affected unit's stat and energy peak's stat
     
     parameters
@@ -446,6 +481,8 @@ def energy_mining(dict_army,dict_order,dict_board):
     dict_army: dictionnary with the unit of the two player(dict)
     dict_order[energy_mining]: dictionnary of energy mining order(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
     
     return
     ------
@@ -454,7 +491,7 @@ def energy_mining(dict_army,dict_order,dict_board):
 
     Version
     −−−−−−−
-    specification: François Bechet (v.1 24/02/20)
+    specification: François Bechet (v.2 24/02/20)
     """
 
 def compute_manhattan_distance(x_shooter,y_shooter,x_target,y_target):
@@ -474,7 +511,7 @@ def compute_manhattan_distance(x_shooter,y_shooter,x_target,y_target):
     -------
     specification: François Bechet (v.1 24/02/20)
     """
-def play_turn(dict_board,dict_army,dict_recruit,dict_ordre):
+def play_turn(dict_board,dict_army,dict_recruit,dict_ordre,player1,player2):
     """ manage each turn of the game by receiving the commands of each player
 
     Parameters
@@ -483,9 +520,11 @@ def play_turn(dict_board,dict_army,dict_recruit,dict_ordre):
     dict_order:dictionnary of players orders(dict)
     dict_board: dictionnary with all the characteristic of the board (dict)
     dict_recruit: dictionnary with research and stat of new ship(dict)
+    player1:name of the first palyer(str)
+    player2:name of the second palyer(str)
 
     Version
     -------
-    specification: François Bechet (v.1 24/02/20)
+    specification: François Bechet (v.2 24/02/20)
     """
 game(True)
